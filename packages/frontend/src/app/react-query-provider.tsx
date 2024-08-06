4ec1147bee791a12e3861b7aec0c3993f7444514
d-mon- <guerin.olivier@outlook.com>
2024-08-06 23:33:39 +0200

    feat: redirect user to login page when not authenticated

--- packages/frontend/src/app/react-query-provider.tsx
+++ packages/frontend/src/app/react-query-provider.tsx
@@ -1,19 +1,63 @@
 'use client';
 
-import axios from 'axios';
-import { useState } from 'react';
+import axios, { AxiosError, AxiosResponse } from 'axios';
+import Cookies from 'js-cookie';
+import { usePathname, useSearchParams } from 'next/navigation';
+import { useRouter } from 'next/navigation';
+import { useEffect, useState } from 'react';
 import { QueryClient, QueryClientProvider } from 'react-query';
 
 import { AxiosQuery } from '@/api';
 
+const listeners = {
+  response: (_v: AxiosResponse<unknown, unknown>) => {
+    return;
+  },
+  error: (_error: unknown) => {
+    return;
+  },
+};
+
 AxiosQuery.setBaseUrl('http://localhost:3001');
 AxiosQuery.setAxiosFactory(() => {
-  return axios.create({ withCredentials: true });
+  const t = axios.create({ withCredentials: true });
+  t.interceptors.response.use(
+    function (response) {
+      listeners.response(response);
+      return response;
+    },
+    function (error) {
+      listeners.error(error);
+      return Promise.reject(error);
+    }
+  );
+  return t;
 });
+
+const allowedPathnames = ['/login'];
 
 const ReactQueryProvider = ({ children }: { children: React.ReactNode }) => {
   const [queryClient] = useState(() => new QueryClient());
 
+  const router = useRouter();
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+
+  useEffect(() => {
+    listeners.error = (v: unknown) => {
+      // Redirect user to login page if not authenticated anymore
+      if (
+        v instanceof AxiosError &&
+        v.response?.statusText === 'Unauthorized' &&
+        !Cookies.get('is_authenticated') &&
+        !allowedPathnames.includes(pathname)
+      ) {
+        router.replace(
+          '/login?prev=' + encodeURIComponent(pathname + '?' + searchParams)
+        );
+      }
+    };
+  }, [pathname, searchParams, router]);
   return (
     <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
   );
